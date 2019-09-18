d97f9c8cc4b9c938f91548dfd1127fb37274a862
zcs18 <zcs18@hampshire.edu>
2019-09-18 11:32:00 -0400

    no message

---
+++ 1.2/Assets/ControlScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlScript : MonoBehaviour
+{
+    int bronze = 0;
+    int silver = 0;
+    int bronzeSupply = 3;
+    int silverSupply = 2;
+    float mineNow;
+    float miningSpeed = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mineNow = miningSpeed; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time>mineNow)
+        {
+            mineNow += miningSpeed;
+            
+            if (bronzeSupply > 0)
+            {
+                bronze++;
+                bronzeSupply--;
+            }
+            else if (silverSupply > 0)
+            {
+                silver++;
+                silverSupply--;
+            }
+            print("Bronze:" + bronze + " silver:" + silver);
+        }
+        
+    }
+}
